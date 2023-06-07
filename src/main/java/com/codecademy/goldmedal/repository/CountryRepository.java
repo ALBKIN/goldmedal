9b64f21008c3bfd2056c846d954eccb3cf8aee79
ALBKIN <kinkel.albert@gmail.com>
2023-06-07 18:25:06 +0200

    07.06.2023

--- src/main/java/com/codecademy/goldmedal/repository/CountryRepository.java
+++ src/main/java/com/codecademy/goldmedal/repository/CountryRepository.java
@@ -2,6 +2,19 @@ package com.codecademy.goldmedal.repository;
 
 import com.codecademy.goldmedal.model.Country;
 import org.springframework.data.repository.CrudRepository;
+import java.util.Optional;
+import java.util.List;
 
-public interface CountryRepository extends CrudRepository {
+public interface CountryRepository extends CrudRepository<Country, Long> {
+    List<Country> getAllByOrderByNameAsc();
+    List<Country> getAllByOrderByNameDesc();
+    List<Country> getAllByOrderByGdpAsc();
+    List<Country> getAllByOrderByGdpDesc();
+    List<Country> getAllByOrderByPopulationAsc();
+    List<Country> getAllByOrderByPopulationDesc();
+    Optional<Country> getByName(String name);
 }
+
+
+
+
